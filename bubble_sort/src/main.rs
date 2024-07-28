417ff68f9899001e4846cc503f1d6bf5eeda8d4c
unknown <olivia.bergaglia@gmail.com>
2024-07-28 22:53:16 +0200

    toujours des tests

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let arr: [i32; 10] = [15, 76, 74, 64, 23, 95, 06, 81, 46, 52];
+
+    // let i: i32;
+    // let j: i32;
+    let _swapped : bool;
+
+    for i in 1..101 {
+        if i % 15 == 0 {
+            println!("fizzbuzz");
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+
+    println!("{:?}", arr)
+
 }
---
+++ bubble_sort/target/debug/deps/bubble_sort-485d617f0e24cb96.d
@@ -0,0 +1,5 @@
+D:\Documents\code\Rust\bubble_sort\bubble_sort\target\debug\deps\libbubble_sort-485d617f0e24cb96.rmeta: src/main.rs
+
+D:\Documents\code\Rust\bubble_sort\bubble_sort\target\debug\deps\bubble_sort-485d617f0e24cb96.d: src/main.rs
+
+src/main.rs:
