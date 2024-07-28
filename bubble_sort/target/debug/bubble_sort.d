2c345cfa6ecd2d595b617ac3926805d643bf48f0
unknown <olivia.bergaglia@gmail.com>
2024-07-28 21:57:47 +0200

    project build

---
+++ bubble_sort/target/debug/bubble_sort.d
@@ -0,0 +1,1 @@
+D:\Documents\code\Rust\bubble_sort\bubble_sort\target\debug\bubble_sort.exe: D:\Documents\code\Rust\bubble_sort\bubble_sort\src\main.rs
---
+++ bubble_sort/target/debug/deps/bubble_sort.d
@@ -0,0 +1,5 @@
+D:\Documents\code\Rust\bubble_sort\bubble_sort\target\debug\deps\bubble_sort.exe: src/main.rs
+
+D:\Documents\code\Rust\bubble_sort\bubble_sort\target\debug\deps\bubble_sort.d: src/main.rs
+
+src/main.rs:
